8be96f3d228675b208270ea31e684e2115f3da53
kendah-bryan <azemafcbryan@gmail.com>
2023-11-20 01:10:01 -0500

    image-search-app.1

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const accessKey = "DXsuCftys3FsuYsyioLjO7H3wZGq6N-pKtUifwhYg0U";
 
-const formEl = document.querySelector("form");
-const inputEl = document.getElementById("search-input");
+const formEl = document.querySelector('form');
+const inputEl = document.getElementById('search-input');
 const searchResults = document.querySelector('.search-results');
 const showMore = document.querySelector('#show-more-button');
 
@@ -9,9 +9,11 @@ const showMore = document.querySelector('#show-more-button');
 let inputData = "";
 let page = 1;
 
+
 async function searchImages() {
+    console.log(page)
     inputData = inputEl.value;
-    const url = `https://unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}`;
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}`;
 
     const response = await fetch(url)
     const data = await response.json()
@@ -24,10 +26,10 @@ async function searchImages() {
 
     results.map((result) => {
         const imageWrapper = document.createElement('div')
-        imageWrapper.classList.add("search-result")
+        imageWrapper.classList.add('search-result')
         const img = document.createElement('img')
-        img.src = results.urls.small
-        img.alt = results.alt_description
+        img.src = result.urls.small
+        img.alt = result.alt_description
         const imgLink = document.createElement('a')
         imgLink.href = result.links.html
         img.target = "_blank";
@@ -51,6 +53,7 @@ formEl.addEventListener("submit", (event) => {
     page = 1;
     searchImages();
 })
+
 showMore.addEventListener("click", () => {
     searchImages()
 })
